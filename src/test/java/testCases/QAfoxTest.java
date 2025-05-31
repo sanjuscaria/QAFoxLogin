2a716a95fa1a76cc7e2b952ddc547a0dbb08898e
sanju <sanju.scaria@gmail.com>
2025-05-31 10:39:28 +0530

    commented all executable lines

--- src/test/java/testCases/QAfoxTest.java
+++ src/test/java/testCases/QAfoxTest.java
@@ -30,24 +30,24 @@ public class QAfoxTest {
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
 			driver.manage().window().maximize();
-			driver.findElement(By.id("input-email")).sendKeys("fname1.lname1@gmail.com");
-			driver.findElement(By.id("input-password")).sendKeys("pass");
-			driver.findElement(By.xpath("//input[@value='Login']")).click();
-			//Assert.assertEquals(driver.getTitle(), "My Account");
-			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-			wait.until(ExpectedConditions.titleContains("My Account"));
-			//My Account
-			WebElement myAccount = driver
-					.findElement(By.xpath("//div[@id='top-links']/ul/li[2]/a/i/following-sibling::span"));
-			wait.until(ExpectedConditions.elementToBeClickable(myAccount));
-			myAccount.click();
-			//driver.findElement(By.xpath("//div[@id='top-links']/ul/li[2]/a/i/following-sibling::span")).click();
-			//log out
-			WebElement logout = driver.findElement(By.xpath("//div[@id='top-links']/ul/li[2]/a/following-sibling::ul/li[5]/a"));
-			wait.until(ExpectedConditions.elementToBeClickable(logout));
-			logout.click();
-			System.out.println("title "+driver.getTitle());
-			System.out.println("count : "+count);
+//			driver.findElement(By.id("input-email")).sendKeys("fname1.lname1@gmail.com");
+//			driver.findElement(By.id("input-password")).sendKeys("pass");
+//			driver.findElement(By.xpath("//input[@value='Login']")).click();
+//			//Assert.assertEquals(driver.getTitle(), "My Account");
+//			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//			wait.until(ExpectedConditions.titleContains("My Account"));
+//			//My Account
+//			WebElement myAccount = driver
+//					.findElement(By.xpath("//div[@id='top-links']/ul/li[2]/a/i/following-sibling::span"));
+//			wait.until(ExpectedConditions.elementToBeClickable(myAccount));
+//			myAccount.click();
+//			//driver.findElement(By.xpath("//div[@id='top-links']/ul/li[2]/a/i/following-sibling::span")).click();
+//			//log out
+//			WebElement logout = driver.findElement(By.xpath("//div[@id='top-links']/ul/li[2]/a/following-sibling::ul/li[5]/a"));
+//			wait.until(ExpectedConditions.elementToBeClickable(logout));
+//			logout.click();
+//			System.out.println("title "+driver.getTitle());
+//			System.out.println("count : "+count);
 			
 			driver.quit();
 		}
