3558a4c1c72a1dcf10e3dcc2604b86679c3c13f1
sanju <sanju.scaria@gmail.com>
2025-06-02 17:10:22 +0530

    second commit today

--- src/test/java/testCases/QAFoxMenuTest.java
+++ src/test/java/testCases/QAFoxMenuTest.java
@@ -40,7 +40,7 @@ public class QAFoxMenuTest {
 					    webDriver -> ((JavascriptExecutor) webDriver)
 					        .executeScript("return document.readyState").equals("complete"));
 				ScreenshotUtil.takeScreenshot(driver, "clickError");
-				//testing				
+				//need to check this line			
 				
 				driver.quit();
 			} catch (Exception e) {
